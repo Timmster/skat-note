7aac1aa8fd77dd7dae490f29d7d35b43917eefea
Timmster <timm.schwemann@googlemail.com>
2021-11-08 15:55:49 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,26 +10,34 @@ export class AppComponent {
   spiele = SPIELE;
   spieler = ['Margrethe', 'Wencke', 'Timm'];
 
-  ergebnisse: Map<string, number> = new Map();
+  // ergebnisse: Map<string, string> = new Map();
 
   constructor() {}
 
   getErgebnis(spieler: string, spiel: SkatSpiel) {
-    return this.ergebnisse.get(spieler + '_' + spiel.name);
+    return localStorage.getItem(spieler + '_' + spiel.name);
+    // return this.ergebnisse.get(spieler + '_' + spiel.name);
   }
 
   setErgebnis(spieler: string, spiel: SkatSpiel, punkte: number) {
-    if (this.getErgebnis(spieler, spiel) == punkte) {
-      this.ergebnisse.delete(spieler + '_' + spiel.name);
+    if (this.isErgebnis(spieler, spiel, punkte)) {
+      // this.ergebnisse.delete(spieler + '_' + spiel.name);
+      localStorage.setItem(spieler + '_' + spiel.name, null);
     } else {
-      this.ergebnisse.set(spieler + '_' + spiel.name, punkte);
+      localStorage.setItem(spieler + '_' + spiel.name, punkte + '');
+      // this.ergebnisse.set(spieler + '_' + spiel.name, punkte + '');
     }
+  }
+
+  isErgebnis(spieler: string, spiel: SkatSpiel, punkte: number) {
+    const pStr = punkte + '';
+    return this.getErgebnis(spieler, spiel) == pStr;
   }
 
   getGesamt(spieler: string) {
     let result = 0;
     this.spiele.forEach((spiel) => {
-      const r = this.getErgebnis(spieler, spiel);
+      const r = Number.parseInt(this.getErgebnis(spieler, spiel));
       result += isNaN(r) ? 0 : r;
     });
     return result;
