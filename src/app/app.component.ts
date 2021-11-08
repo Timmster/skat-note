24053853e3d06fc2ce029905bf6ee6ba15a616f2
Timmster <timm.schwemann@googlemail.com>
2021-11-08 15:40:05 +0100

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,4 +17,18 @@ export class AppComponent {
   getErgebnis(spieler: string, spiel: SkatSpiel) {
     return this.ergebnisse.get(spieler + '_' + spiel.name);
   }
+
+  setErgebnis(spieler: string, spiel: SkatSpiel, punkte: number) {
+    this.ergebnisse.delete(spieler + '_' + spiel.name);
+    this.ergebnisse.set(spieler + '_' + spiel.name, punkte);
+  }
+
+  getGesamt(spieler: string) {
+    let result = 0;
+    this.spiele.forEach((spiel) => {
+      const r = this.getErgebnis(spieler, spiel);
+      result += isNaN(r) ? 0 : r;
+    });
+    return result;
+  }
 }
